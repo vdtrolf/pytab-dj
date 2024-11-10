6876de88cfd0f7fbe2deebe153e2e726f229822d
Rolf <vdt.rolf@xs4all.nl>
2024-11-10 04:31:58 +0100

    tests

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hello_world_project project.
+URL configuration for django_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 # pages/views.py
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+  template_name = "home.html"
+  
+class AboutPageView(TemplateView):
+  template_name = "about.html"
